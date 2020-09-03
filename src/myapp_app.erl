20445a128ff566ee85e5406ca229e33a62e1d097
sirarthurnell <sirarthurnell@gmail.com>
2020-09-03 18:13:25 +0200

    First commit

---
+++ src/myapp_app.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%% @doc myapp public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(myapp_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    io:format("Hello world"),
+    myapp_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
